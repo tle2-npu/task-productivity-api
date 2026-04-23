9760690cdce965bcdb8429801425c996c01f9e96
tle2-npu <tle2@northpark.edu>
2026-04-23 17:50:01 -0500

    add logging and global error handler middleware

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 const express = require("express");
 require("dotenv").config();
 
+const logger = require("./src/middleware/logger.middleware");
+const errorHandler = require("./src/middleware/error.middleware");
+
 const app = express();
 
 app.use(express.json());
+app.use(logger);
 
-// test route
 app.get("/", (req, res) => {
   res.json({ message: "Task Productivity API is running!" });
 });
+
+// error middleware
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3000;
 
